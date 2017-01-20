3c1e9970dc9c36d182d19762dcdf33cfd953d3b5
agatan <agatan039@gmail.com>
2017-01-20 22:41:52 +0900

    Update benchmark

--- benches/lib.rs
+++ benches/lib.rs
@@ -1,6 +1,5 @@
 #![feature(test)]
 extern crate test;
-extern crate rand;
 
 use test::Bencher;
 
@@ -9,15 +8,12 @@ use yoin::op;
 
 #[bench]
 fn bench_lookup(b: &mut Bencher) {
-    let mut rng = rand::thread_rng();
     let words = include_str!("./words.txt");
     let words: Vec<_> = words.lines().collect();
-    let sample = rand::sample(&mut rng, words, 1000);
     let bytes = include_bytes!("../mecab.dic");
-    let sample = &sample;
     b.iter(|| {
-        for input in sample {
-            op::run(bytes, input.as_bytes()).unwrap();
+        for word in &words {
+            op::run(bytes, word.as_bytes()).unwrap();
         }
-    })
+    });
 }
