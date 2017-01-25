67bc0f396f5d2fba15fa5f812dc1e48f09988328
agatan <agatan039@gmail.com>
2017-01-25 21:29:21 +0900

    Move data binaries

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 extern crate byteorder;
 extern crate yoin_fst as fst;
+extern crate yoin_ipadic as ipadic;
 
 pub mod morph;
 pub mod dict;
--- yoin-ipadic/src/lib.rs
+++ yoin-ipadic/src/lib.rs
@@ -1,6 +1,4 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-    }
-}
+extern crate yoin_fst as fst;
+
+pub const BYTECODE: &'static [u8] = include_bytes!("../data/mecab.dic");
+pub const ENTRIES: &'static [u8] = include_bytes!("../data/mecab.entries");
