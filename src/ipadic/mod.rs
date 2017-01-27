448d18a1d4efec3be4b526049daf4efc5858eed0
agatan <agatan039@gmail.com>
2017-01-27 22:42:27 +0900

    Add ipadic::matrix

--- src/ipadic/mod.rs
+++ src/ipadic/mod.rs
@@ -1,4 +1,5 @@
 use dict::Dict;
+use matrix::Matrix;
 
 pub const BYTECODE: &'static [u8] = include_bytes!("../../data/ipadic.dic");
 pub const MORPHS: &'static [u8] = include_bytes!("../../data/ipadic.morph");
@@ -6,4 +7,8 @@ pub const MATRIX: &'static [u8] = include_bytes!("../../data/ipadic.matrix");
 
 pub fn dictionary() -> Dict<&'static [u8]> {
     unsafe { Dict::from_bytes(BYTECODE, MORPHS) }
+}
+
+pub fn matrix() -> Matrix<&'static [i16]> {
+    unsafe { Matrix::decode(MATRIX) }
 }
