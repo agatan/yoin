af2701f35fa2a308a902080576a0396a0ee35019
agatan <agatan039@gmail.com>
2017-01-27 22:40:04 +0900

    Use ipadic::dictionary

--- src/bin/yoin.rs
+++ src/bin/yoin.rs
@@ -5,15 +5,12 @@ use std::env;
 
 extern crate yoin;
 
-use yoin::dict::Dict;
 use yoin::ipadic;
 
 fn main() {
-    let bytecodes = ipadic::BYTECODE;
-    let morphs = ipadic::MORPHS;
-    let dict = unsafe { Dict::from_bytes(bytecodes, morphs) };
+    let dict = ipadic::dictionary();
     let input = env::args().nth(1).unwrap();
-    let morphs = dict.lookup(input.as_bytes()).unwrap();
+    let morphs = dict.lookup_str(input.as_str()).unwrap();
     for morph in &morphs {
         println!("{}", morph);
     }
