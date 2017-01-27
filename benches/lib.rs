4033d4e32be5654d0d3a63c5073869293e974208
agatan <agatan039@gmail.com>
2017-01-27 22:38:21 +0900

    Define dictionary in ipadic

--- benches/lib.rs
+++ benches/lib.rs
@@ -4,16 +4,16 @@ extern crate test;
 use test::Bencher;
 
 extern crate yoin;
+use yoin::ipadic;
 
 #[bench]
 fn bench_lookup(b: &mut Bencher) {
     let words = include_str!("./words.txt");
     let words: Vec<_> = words.lines().collect();
-    let bytes = include_bytes!("../data/mecab.dic");
-    let fst = unsafe { yoin::fst::Fst::from_bytes(bytes) };
+    let dict = ipadic::dictionary();
     b.iter(|| {
         for word in &words {
-            fst.run(word.as_bytes()).unwrap();
+            dict.lookup_str(word).unwrap();
         }
     });
 }
