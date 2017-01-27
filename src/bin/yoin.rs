e09401272b0f9d080ef0fc7fde44f318a45be210
agatan <agatan039@gmail.com>
2017-01-27 20:46:00 +0900

    Remove Dict trait

--- src/bin/yoin.rs
+++ src/bin/yoin.rs
@@ -5,13 +5,13 @@ use std::env;
 
 extern crate yoin;
 
-use yoin::dict::{Dict, DecodedDict};
+use yoin::dict::Dict;
 use yoin::ipadic;
 
 fn main() {
     let bytecodes = ipadic::BYTECODE;
     let entries = ipadic::ENTRIES;
-    let dict = unsafe { DecodedDict::from_bytes(bytecodes, entries) };
+    let dict = unsafe { Dict::from_bytes(bytecodes, entries) };
     let input = env::args().nth(1).unwrap();
     let morphs = dict.lookup(input.as_bytes()).unwrap();
     for morph in &morphs {
