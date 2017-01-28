d3ab4081947ebf3e68c34615fc1137088f34fe91
agatan <agatan039@gmail.com>
2017-01-28 17:06:03 +0900

    Calc connection_cost

--- src/ipadic/mod.rs
+++ src/ipadic/mod.rs
@@ -5,8 +5,8 @@ pub const BYTECODE: &'static [u8] = include_bytes!("../../data/ipadic.dic");
 pub const MORPHS: &'static [u8] = include_bytes!("../../data/ipadic.morph");
 pub const MATRIX: &'static [u8] = include_bytes!("../../data/ipadic.matrix");
 
-pub fn dictionary() -> FstDict<&'static [u8]> {
-    unsafe { FstDict::from_bytes(BYTECODE, MORPHS) }
+pub fn dictionary() -> FstDict<&'static [u8], &'static [i16]> {
+    unsafe { FstDict::from_bytes(BYTECODE, MORPHS, MATRIX) }
 }
 
 pub fn matrix() -> Matrix<&'static [i16]> {
