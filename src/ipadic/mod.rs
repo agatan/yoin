eba9b8283d721c5b6d29ca38ee522da1c84758a3
agatan <agatan039@gmail.com>
2017-01-29 12:59:20 +0900

    Define Tokenizer

--- src/ipadic/mod.rs
+++ src/ipadic/mod.rs
@@ -1,4 +1,5 @@
 use dict::{FstDict, Matrix};
+use tokenizer::Tokenizer;
 
 pub const BYTECODE: &'static [u8] = include_bytes!("../../data/ipadic.dic");
 pub const MORPHS: &'static [u8] = include_bytes!("../../data/ipadic.morph");
@@ -10,4 +11,8 @@ pub fn dictionary() -> FstDict<&'static [u8], &'static [i16]> {
 
 pub fn matrix() -> Matrix<&'static [i16]> {
     unsafe { Matrix::decode(MATRIX) }
+}
+
+pub fn tokenizer() -> Tokenizer<'static, FstDict<&'static [u8], &'static [i16]>> {
+    Tokenizer::new_with_dic(dictionary())
 }
