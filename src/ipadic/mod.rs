03c3835ab102f3aa1df77dd5ca636175b1923a87
agatan <agatan039@gmail.com>
2017-01-29 23:17:09 +0900

    Add unknown dictionary of ipadic

--- src/ipadic/mod.rs
+++ src/ipadic/mod.rs
@@ -1,9 +1,11 @@
 use dict::{FstDict, Matrix};
+use dict::unknown::CompiledUnkDict;
 use tokenizer::Tokenizer;
 
 pub const BYTECODE: &'static [u8] = include_bytes!("../../data/ipadic.dic");
 pub const MORPHS: &'static [u8] = include_bytes!("../../data/ipadic.morph");
 pub const MATRIX: &'static [u8] = include_bytes!("../../data/ipadic.matrix");
+pub const UNKOWN: &'static [u8] = include_bytes!("../../data/ipadic.unk");
 
 pub fn dictionary() -> FstDict<&'static [u8], &'static [i16]> {
     unsafe { FstDict::from_bytes(BYTECODE, MORPHS, MATRIX) }
@@ -13,6 +15,10 @@ pub fn matrix() -> Matrix<&'static [i16]> {
     unsafe { Matrix::decode(MATRIX) }
 }
 
-pub fn tokenizer() -> Tokenizer<'static, FstDict<&'static [u8], &'static [i16]>> {
-    Tokenizer::new_with_dic(dictionary())
+pub fn unkown_dic() -> CompiledUnkDict<'static> {
+    unsafe { CompiledUnkDict::decode(UNKOWN) }
+}
+
+pub fn tokenizer() -> Tokenizer<'static, FstDict<&'static [u8], &'static [i16]>, CompiledUnkDict<'static>> {
+    Tokenizer::new_with_dic(dictionary(), unkown_dic())
 }
