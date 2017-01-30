4b16187ed2304be86270869fcba394a59cad3ae0
agatan <agatan039@gmail.com>
2017-01-30 18:15:27 +0900

    Add doctest

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,20 @@
+//! A Japanese morphological analyzer written in pure Rust.
+//!
+//! ## Examples
+//!
+//! ```
+//! use yoin::ipadic;
+//!
+//! let input = "すもももももももものうち";
+//! let expected = vec!["すもも", "も", "もも", "も", "もも", "の", "うち"];
+//!
+//! let tokenizer = ipadic::tokenizer();
+//! let tokens = tokenizer.tokenize(input);
+//!
+//! for (tok, e) in tokens.iter().zip(expected) {
+//!     assert_eq!(tok.surface(), e);
+//! }
+//! ```
 extern crate byteorder;
 
 pub mod dic;
