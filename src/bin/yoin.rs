ea9942c741dd69f8b88dab073e2e6886a6c2211e
agatan <agatan039@gmail.com>
2017-01-30 17:34:51 +0900

    Dump lattice graph

--- src/bin/yoin.rs
+++ src/bin/yoin.rs
@@ -9,14 +9,20 @@ extern crate yoin;
 
 use yoin::ipadic;
 
-fn read_and_analyze_lines<R: io::BufRead>(r: R) -> io::Result<()> {
+fn read_and_analyze_lines<R: io::BufRead>(mut r: R, lattice: bool) -> io::Result<()> {
     let tokenizer = ipadic::tokenizer();
-    for line in r.lines() {
-        let line = line?;
-        for node in tokenizer.tokenize(line.as_str()) {
-            println!("{}", node);
+    if !lattice {
+        for line in r.lines() {
+            let line = line?;
+            for node in tokenizer.tokenize(line.as_str()) {
+                println!("{}", node);
+            }
+            println!("EOS");
         }
-        println!("EOS");
+    } else {
+        let mut buf = String::new();
+        r.read_to_string(&mut buf)?;
+        tokenizer.dump_lattice(buf.trim(), io::stdout())?
     }
     Ok(())
 }
@@ -30,13 +36,16 @@ fn main() {
             .value_name("FILE")
             .help("input file. if not specified, read from stdin")
             .takes_value(true))
+        .arg(Arg::with_name("lattice").long("lattice").help("dump lattice as dot format"))
         .get_matches();
+
+    let lattice = matches.is_present("lattice");
 
     if let Some(file) = matches.value_of("file") {
         let file = File::open(file).unwrap();
-        read_and_analyze_lines(io::BufReader::new(file)).unwrap();
+        read_and_analyze_lines(io::BufReader::new(file), lattice).unwrap();
     } else {
         let stdin = io::stdin();
-        read_and_analyze_lines(stdin.lock()).unwrap();
+        read_and_analyze_lines(stdin.lock(), lattice).unwrap();
     }
 }
