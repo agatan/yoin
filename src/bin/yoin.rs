f08d2985793cefe6b1943e96ce8732fa709af891
agatan <agatan039@gmail.com>
2017-01-30 16:27:36 +0900

    Read from file

--- src/bin/yoin.rs
+++ src/bin/yoin.rs
@@ -1,21 +1,41 @@
-extern crate encoding;
-extern crate byteorder;
+extern crate clap;
 
-use std::io::prelude::*;
 use std::io;
+use std::fs::File;
+
+use clap::{Arg, App};
 
 extern crate yoin;
 
 use yoin::ipadic;
 
-fn main() {
+fn read_and_analyze_lines<R: io::BufRead>(r: R) -> io::Result<()> {
     let tokenizer = ipadic::tokenizer();
-    let stdin = io::stdin();
-    for line in stdin.lock().lines() {
-        let line = line.unwrap();
+    for line in r.lines() {
+        let line = line?;
         for node in tokenizer.tokenize(line.as_str()) {
             println!("{}", node);
         }
         println!("EOS");
+    }
+    Ok(())
+}
+
+fn main() {
+    let matches = App::new("yoin")
+        .about("Japanese Morphological Analyzer")
+        .arg(Arg::with_name("file")
+            .long("file")
+            .value_name("FILE")
+            .help("input file. if not specified, read from stdin")
+            .takes_value(true))
+        .get_matches();
+
+    if let Some(file) = matches.value_of("file") {
+        let file = File::open(file).unwrap();
+        read_and_analyze_lines(io::BufReader::new(file)).unwrap();
+    } else {
+        let stdin = io::stdin();
+        read_and_analyze_lines(stdin.lock()).unwrap();
     }
 }
