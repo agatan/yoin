5e80f16c3c8660d2e3c8d53b27fd76acf580ebca
agatan <agatan039@gmail.com>
2017-01-31 22:39:33 +0900

    Remove ipadic module

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,8 @@
 //! A Japanese morphological analyzer written in pure Rust.
-//!
-//! ## Examples
-//!
-//! ```
-//! use yoin::ipadic;
-//!
-//! let input = "すもももももももものうち";
-//!
-//! let tokenizer = ipadic::tokenizer();
-//!
-//! for token in tokenizer.tokenize(input) {
-//!     println!("{}", token.surface());
-//!     println!("{}", &input[token.start()..token.end()]);
-//!     for feature in token.features() {
-//!         println!("{}", feature);
-//!     }
-//! }
-//! ```
 extern crate byteorder;
 
 pub mod dic;
 pub mod sysdic;
-#[cfg(feature = "ipadic")]
-pub mod ipadic;
 pub mod tokenizer;
 
 pub const VERSION: &'static str = "0.0.1";
