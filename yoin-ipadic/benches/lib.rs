bf008cab54b190d798ff29afafdfcad9baa2b67c
agatan <agatan039@gmail.com>
2017-02-02 00:19:24 +0900

    Fix broken benchmark code

--- yoin-ipadic/benches/lib.rs
+++ yoin-ipadic/benches/lib.rs
@@ -3,9 +3,9 @@ extern crate test;
 
 use test::Bencher;
 
-extern crate yoin;
-use yoin::dic::Dic;
-use yoin::ipadic;
+extern crate yoin_ipadic as ipadic;
+extern crate yoin_core as core;
+use core::dic::Dic;
 
 #[bench]
 fn bench_lookup(b: &mut Bencher) {
