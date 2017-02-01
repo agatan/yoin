f210befc45e72a65bb4e62f6317ef9ee2fa27b60
agatan <agatan039@gmail.com>
2017-02-01 17:55:42 +0900

    Compress data with bzip2 and decompress them in build.rs

---
+++ build.rs
@@ -0,0 +1,26 @@
+extern crate bzip2;
+extern crate tar;
+
+use std::io::prelude::*;
+use std::fs::{self, File};
+use std::path::Path;
+
+use bzip2::read::BzDecoder;
+use tar::Archive;
+
+fn main() {
+    let data_dir = Path::new("./data");
+    if data_dir.is_dir() {
+        return;
+    }
+
+    let compressed = File::open("./data.tar.bz2").unwrap();
+    let decoder = BzDecoder::new(compressed);
+    let mut archive = Archive::new(decoder);
+
+    for file in archive.entries().unwrap() {
+        let mut file = file.unwrap();
+
+        file.unpack_in(".").unwrap();
+    }
+}
